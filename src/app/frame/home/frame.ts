2717e586cca0ba54d22449a734065f6f7575236f
Aqua Dolf <aaqddf@icloud.com>
2024-02-14 20:37:08 +1100

    set frame.image

--- src/app/frame/home/frame.ts
+++ src/app/frame/home/frame.ts
@@ -3,7 +3,7 @@ import { AppConfig } from "../../AppConfig";
 
 export function HomeFrame() {
     const frame: Frame = {
-        image: "",
+        image: AppConfig.hostUrl + '/image/home',
         postUrl: AppConfig.hostUrl + "/frame/home",
         version: "vNext",
         buttons: [
