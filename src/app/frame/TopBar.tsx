71f0cfa72aa190489054eae5d515837795092cbd
Aqua Dolf <aaqddf@icloud.com>
2024-02-15 17:17:53 +1100

    init TopBar jsx

---
+++ src/app/frame/TopBar.tsx
@@ -0,0 +1,47 @@
+type TopBarProps = {
+    children?: JSX.Element[] | JSX.Element
+    justifyContent?: string
+    pfpUrl?: string | null
+    route?: string
+    pfpSize?: number
+}
+export default function TopBar(props: TopBarProps) {
+    const {pfpUrl, route, justifyContent, children} = props
+    const pfpSize = props.pfpSize ?? 100
+    return (
+                <div
+                    id="top-bar"
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "100%",
+                        fontFamily: "mono",
+                        justifyContent: justifyContent ?? 'space-between',
+                    }}
+                >
+                    {route ? (
+                        <div id="current-route">
+                            {"/" + route}
+                        </div>
+                    ):null}
+                    {pfpUrl ? (
+                        <img
+                            alt="pfp"
+                            src={pfpUrl}
+                            width={pfpSize}
+                            height={pfpSize}
+                            style={{
+                                borderRadius: pfpSize / 2,
+                                position: "absolute",
+                                top: 0,
+                                left: "50%",
+                                transform: "translate(-50%, 0)",
+                            }}
+                        />
+                    ) : null}
+                    {children ? (
+                        children
+                    ):null}
+                </div>
+    )
+}
