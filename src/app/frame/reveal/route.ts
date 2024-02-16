8dca8f12900f171b4edcd345cecf5fc0475f46a6
Aqua Dolf <aaqddf@icloud.com>
2024-02-17 09:43:01 +1100

    no return

--- src/app/frame/reveal/route.ts
+++ src/app/frame/reveal/route.ts
@@ -68,7 +68,7 @@ export async function POST(req: NextRequest) {
                 } as MyCastsFrameParams,
             });
         } else {
-            MainMenuFrame(data.untrustedData.fid);
+            return MainMenuFrame(data.untrustedData.fid);
         }
     }
 }
