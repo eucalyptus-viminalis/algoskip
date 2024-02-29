2b1d86326e1c7c56a75e4dbd65f674ef80eb8e9c
Aqua Dolf <aaqddf@icloud.com>
2024-02-29 19:09:18 +1100

    add GET route

--- src/app/frame/select-channel/route.ts
+++ src/app/frame/select-channel/route.ts
@@ -7,6 +7,44 @@ import {
     TrendingCastsFrameParams,
 } from "../trending-casts/frame";
 import { ErrorFrame } from "../error/frame";
+import { ChannelFilter } from "./data";
+
+export async function GET(req: NextRequest) {
+    const channelFilter = req.nextUrl.searchParams.get('channelFilter')
+    const fid = req.nextUrl.searchParams.get('fid')
+    const skip = req.nextUrl.searchParams.get('skip')
+    const filters = req.nextUrl.searchParams.get('filters')
+    const pfpUrl = req.nextUrl.searchParams.get('pfpUrl')
+    const username = req.nextUrl.searchParams.get('username')
+    const algo = req.nextUrl.searchParams.get('algo')
+    const channel = req.nextUrl.searchParams.get('channel')
+    if (!channelFilter) {
+        return new Response('missing param: channelFilter', {status: 400})
+    } else if (!fid) {
+        return new Response('missing param: fid', {status: 400})
+    } else if (!pfpUrl) {
+        return new Response('missing param: pfpUrl', {status: 400})
+    } else if (!username) {
+        return new Response('missing param: username', {status: 400})
+    }
+    const params: SelectChannelFrameParams = {
+        channelFilter: channelFilter as ChannelFilter,
+        fid: +fid,
+        skip: skip ? + skip : 0,
+        trendingCastsFrameParams: {
+            filters: {
+                embeds: filters?.includes('embeds') ?? false,
+                followerReactions: filters?.includes('followerReactions') ?? false,
+                mentions: filters?.includes('mentions') ?? false,
+            },
+            pfpUrl,
+            username,
+            algo: algo ?? undefined,
+            channel: channel ?? undefined,
+        } as TrendingCastsFrameParams,
+    }
+    return SelectChannelFrame(params)
+}
 
 export async function POST(req: NextRequest) {
     // Params
